88d551d5700142012140c0a546d589ce22774295
Radad-J <radad1998@gmail.com>
2021-08-29 14:11:26 +0200

    Add seeders for categories/cat_pack/packs/stores/roles/reservations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,14 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->call([
+            RolesTableSeeder::class,
+            CategoriesTableSeeder::class,
+            StoresTableSeeder::class,
+            UsersTableSeeder::class,
+            PacksTableSeeder::class,
+            ReservationsTableSeeder::class,
+            CategoryPackTableSeeder::class,
+        ]);
     }
 }
---
+++ database/seeders/RolesTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class RolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //Empty the table first
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        DB::table('roles')->truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1');
+        //Fill data
+        DB::table('roles')->insert([
+            ['role'=>'Admin'],
+            ['role'=>'Member'],
+        ]);
+    }
+}
