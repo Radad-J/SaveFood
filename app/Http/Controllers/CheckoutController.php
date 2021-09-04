3e30071212f273c92eedd8f37cbf744969141b54
Radad-J <radad1998@gmail.com>
2021-09-04 12:39:52 +0200

    Add cart and checkout routes

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -2,83 +2,60 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pack;
+use App\Models\Reservation;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Darryldecode\Cart\Cart;
+use Illuminate\Support\Facades\Auth;
+use Stripe\Charge;
+use Stripe\Customer;
+use Stripe\Stripe;
 
 class CheckoutController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function charge(Request $request)
     {
-        return view('checkout.index');
-    }
+        try {
+            Stripe::setApiKey(env('STRIPE_SECRET_KEY'));
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+            $customer = Customer::create(array(
+                'email' => $request->stripeEmail,
+                'source' => $request->stripeToken
+            ));
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+            $charge = Charge::create(array(
+                'customer' => $customer->id,
+                'amount' => \Cart::getTotal() * 100,
+                'currency' => 'usd'
+            ));
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+            foreach(\Cart::getContent() as $item){
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+                $pack = Pack::find($item->id);
+                if($pack) $pack->stock = $pack->stock - $item->quantity;
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
+                if ($pack->stock < 0){
+                    return redirect()->route('welcome')->with('error', 'Sorry! not enough in stock, please come back later');
+                }else{
+                        $pack->stock = $pack->stock;
+                        $pack->save();
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+                    Reservation::create([
+                        'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                        'user_id' => Auth::check(),
+                        'pack_id' => $item->id,
+                        'quantity' => $item->quantity,
+                        'status' => 'not claimed',
+                    ]);
+                }
+            }
+
+            \Cart::clear();
+
+            return redirect()->route('welcome')->with('success', 'The payment was successful !');
+        } catch (\Exception $e) {
+            return redirect()->route('welcome')->with('error', 'There was a problem with the payment : ' . $e->getMessage());
+        }
     }
 }
---
+++ resources/views/cart/index.blade.php
@@ -0,0 +1,130 @@
+@extends('layouts.app')
+@section('content')
+
+    <section class="breadcrumbs-custom">
+        <div class="parallax-container" data-parallax-img="images/bg-about-2.jpg">
+            <div class="breadcrumbs-custom-body parallax-content context-dark">
+                <div class="container">
+                    <h2 class="text-transform-capitalize breadcrumbs-custom-title">Cart Page</h2>
+                    <h5 class="breadcrumbs-custom-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                        eius <br class="d-none d-md-block">mod tempor incididunt ut labore et dolore magna aliqua.</h5>
+                </div>
+            </div>
+        </div>
+        <div class="breadcrumbs-custom-footer">
+            <div class="container">
+                <ul class="breadcrumbs-custom-path">
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="grid-shop.html">Shop</a></li>
+                    <li class="active">Cart Page</li>
+                </ul>
+            </div>
+        </div>
+    </section>
+    @if(!Cart::isEmpty())
+        <!-- Shopping Cart-->
+        <section class="section section-xl bg-default">
+            <div class="container">
+                <!-- shopping-cart-->
+                <div class="table-custom-responsive">
+                    <table class="table-custom table-cart">
+                        <thead>
+                        <tr>
+                            <th>Product name</th>
+                            <th>Price</th>
+                            <th>Quantity</th>
+                            <th>Total</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+
+                        @foreach(Cart::getContent() as $item)
+                            <tr>
+                                <td><a class="table-cart-figure" href="{{route('pack.show', $item->id)}}"><img
+                                            src="{{asset('images/uploads/packs/'.$item->attributes->picture)}}" alt=""
+                                            width="146" height="132"/></a><a class="table-cart-link"
+                                                                             href="single-product.html">{{$item->name}}</a>
+                                </td>
+                                <td>{{$item->price}}€</td>
+                                <td>
+                                    <div class="table-cart-stepper">
+                                        <input class="input-quantity form-input" id="quantity-{{$item->id}}" type="number" data-zeros="true"
+                                               value="{{$item->quantity}}" min="1" max="1000">
+                                    </div>
+                                </td>
+                                <td>{{Cart::get($item->id)->getPriceSum()}}</td>
+                                <td>
+
+                                    <button onclick="updatecart(this)" data-id="{{$item->id}}"
+                                            type="submit" class="btn update-item fa fa-refresh"></button>
+
+                                </td>
+                                <td>
+                                    <form action="{{route('cart.removeitem')}}" method="post">
+                                        @csrf
+                                        <input type="hidden" name="id" value="{{$item->id}}">
+                                        <button type="submit" class="btn fa fa-trash-o"></button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+
+                <div class="group-xl group-justify justify-content-center justify-content-md-between">
+                    <div>
+                        <form method="post" action="{{route('cart.clear')}}"
+                              class="rd-form rd-mailform rd-form-inline rd-form-coupon">
+                            @csrf
+                            <div class="form-button">
+                                <button class="button button-lg button-secondary button-zakaria" type="submit">Empty
+                                    cart
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                    <div>
+                        <div class="group-xl group-middle">
+                            <div>
+                                <div class="group-md group-middle">
+                                    <div class="heading-5 font-weight-medium text-gray-500">Total</div>
+                                    <div class="heading-3 font-weight-normal">{{Cart::getTotal()}}€</div>
+                                </div>
+                            </div>
+                            <a class="button button-lg button-primary button-zakaria" href="{{route('cart.checkout')}}">Proceed to
+                                checkout</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+    @else
+        <h5 class="my-5">You have an empty cart try to find something <a style="text-decoration-line: underline"
+                                                                         href={{route('shop.index')}}>here</a></h5>
+    @endif
+@endsection
+<script type="text/javascript">
+    function updatecart(e) {
+        var idQty = e.getAttribute("data-id");
+        var qtyname = 'quantity-'+ idQty;
+        $.ajax({
+            url: '{{ route('cart.updateitem') }}',
+            method: "patch",
+            data: {
+                _token: '{{ csrf_token() }}', id: e.getAttribute("data-id"), quantity:
+                document.getElementById(qtyname).value
+            },
+            success: function (response) {
+               if (response == true) {
+                    window.location.href = '/cart';
+                } else {
+                   console.log('error');
+                }
+            }
+        });
+    }
+
+</script>
+
