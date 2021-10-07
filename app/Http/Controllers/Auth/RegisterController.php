5dd494d766df4782169fa30459ea8faef4fab9d9
Radad-J <radad1998@gmail.com>
2021-10-07 22:09:11 +0200

    Add validation to checkbox in the register controller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -55,6 +55,8 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'age' => ['accepted'],
+            'terms_and_conditions' => ['accepted'],
         ]);
     }
 
