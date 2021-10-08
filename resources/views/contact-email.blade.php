2fc8ca08084e66466d3cf724d40210d627c62919
Radad-J <radad1998@gmail.com>
2021-10-09 00:20:56 +0200

    Contact us views

---
+++ resources/views/contact-email.blade.php
@@ -0,0 +1,9 @@
+<h2>Hello Admin,</h2><br>
+You received an email from : {{ $name }}<br>
+Here are the details:<br>
+<b>Name:</b> {{ $name }}<br>
+<b>Email:</b> {{ $email }}<br>
+<b>Phone Number:</b> {{ $phone_number }}<br>
+<b>Subject:</b> {{ $subject }}<br>
+<b>Message:</b> {{ $user_message }}<br>
+Thank You
